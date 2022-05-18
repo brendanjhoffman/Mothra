e8a7ae4b398d50d056d485feb8e8214afcba9959
Anthony TRON <anthotfr@gmail.com>
2022-05-18 22:27:49 +0200

    feat(Button) add variant 'contained' | 'outlined' and colors 'primary' | 'secondary'

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,9 +1,32 @@
+import clsx from 'clsx';
 import React from 'react';
 
-const Button = ({ children, ...props }) => {
+const Button = ({ color, variant, children, ...props }) => {
   return (
     <button
-      className="bg-blue-500 text-white px-3 py-1 rounded-full hover:bg-blue-400 active:bg-blue-600"
+      className={clsx(
+        {
+          'ring-1': variant === 'outlined',
+
+          'text-blue-500': color === 'primary' && variant === undefined,
+          'text-gray-600': color === 'secondary' && variant === undefined,
+
+          'text-blue-500 ring-blue-500 ring-1':
+            color === 'primary' && variant === 'outlined',
+
+          'text-white bg-blue-500':
+            color === 'primary' && variant === 'contained',
+
+          'text-gray-600 ring-gray-600 ring-1':
+            color === 'secondary' && variant === 'outlined',
+
+          'text-white bg-gray-600':
+            color === 'secondary' && variant === 'contained',
+        },
+        'px-3 py-1 rounded-full',
+        'active:opacity-30',
+        'transition-opacity',
+      )}
       {...props}
     >
       {children}
--- components/Button/Button.stories.jsx
+++ components/Button/Button.stories.jsx
@@ -12,4 +12,39 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   children: 'Primary',
+  color: 'primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  children: 'Secondary',
+  color: 'secondary',
+};
+
+export const PrimaryContained = Template.bind({});
+PrimaryContained.args = {
+  children: 'Primary',
+  color: 'primary',
+  variant: 'contained',
+};
+
+export const PrimaryOutlined = Template.bind({});
+PrimaryOutlined.args = {
+  children: 'Primary',
+  color: 'primary',
+  variant: 'outlined',
+};
+
+export const SecondaryContained = Template.bind({});
+SecondaryContained.args = {
+  children: 'Secondary',
+  color: 'secondary',
+  variant: 'contained',
+};
+
+export const SecondaryOutlined = Template.bind({});
+SecondaryOutlined.args = {
+  children: 'Secondary',
+  color: 'secondary',
+  variant: 'outlined',
 };
